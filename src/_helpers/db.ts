3e7119356fc15437360e4b4799072693e1927070
Jacques Fourie <jacques@fourie.ca>
2022-12-05 11:23:10 -0700

    Made database able to connect to mongodb atlas

--- src/_helpers/db.ts
+++ src/_helpers/db.ts
@@ -1,17 +1,23 @@
 import mongoose from 'mongoose';
 import {config} from '../configuration/config.js';
 
-if (config.database.user !== ''){
+if (config.database.url === 'localhost' && config.database.user !== '') {
     mongoose.connect(`mongodb://${config.database.user}:${config.database.pass}@${config.database.url}:${config.database.port}/${config.database.name}`);
-} else{
+} else if (config.database.url === 'localhost') {
     mongoose.connect(`mongodb://${config.database.url}:${config.database.port}/${config.database.name}`);
+} else {
+    mongoose.connect(`mongodb+srv://${config.database.user}:${config.database.pass}@${config.database.url}/${config.database.name}?retryWrites=true&w=majority`);
 }
+
 const db = mongoose.connection;
 db.on('error', (error) => {
     logger.error(error)
 });
+db.on('', (error) => {
+    logger.error(error)
+});
 db.once('open', function callback () {
-  logger.info("Mongodb connection is open");
+  logger.info(`Mongodb connection is open to ${config.database.url}/${config.database.name}`);
 });
 
 import account from '../_models/account.model.js';
