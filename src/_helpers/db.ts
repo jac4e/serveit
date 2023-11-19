ce9204356d5814c98ed3ab82073a081ac45578aa
Jacques Fourie <jacques@fourie.ca>
2023-11-19 03:48:39 -0700

    [Hotfix] Correctly create mongodb url

--- src/_helpers/db.ts
+++ src/_helpers/db.ts
@@ -1,13 +1,11 @@
 import mongoose from 'mongoose';
 import {__envConfig} from '../configuration/config.js';
 
-if (__envConfig.database.url === 'localhost' && __envConfig.database.user !== '') {
-    mongoose.connect(`mongodb://${__envConfig.database.user}:${__envConfig.database.pass}@${__envConfig.database.url}:${__envConfig.database.port}/${__envConfig.database.name}`);
-} else if (__envConfig.database.url === 'localhost') {
-    mongoose.connect(`mongodb://${__envConfig.database.url}:${__envConfig.database.port}/${__envConfig.database.name}`);
-} else {
-    mongoose.connect(`mongodb+srv://${__envConfig.database.user}:${__envConfig.database.pass}@${__envConfig.database.url}/${__envConfig.database.name}?retryWrites=true&w=majority`);
-}
+const srv_string = __envConfig.database.url === 'localhost' ? '' : '+srv';
+const account_string = __envConfig.database.user === '' ? '' : `${__envConfig.database.user}:${__envConfig.database.pass}@`;
+const url_string = __envConfig.database.port === '' ? __envConfig.database.url : `${__envConfig.database.url}:${__envConfig.database.port}`;
+
+mongoose.connect(`mongodb${srv_string}://${account_string}${url_string}/${__envConfig.database.name}?retryWrites=true&w=majority`);
 
 const db = mongoose.connection;
 db.on('error', (error) => {
@@ -17,7 +15,7 @@ db.on('', (error) => {
     logger.error(error)
 });
 db.once('open', function callback () {
-  logger.info(`Mongodb connection is open to ${__envConfig.database.url}/${__envConfig.database.name}`);
+  logger.info(`Mongodb connection is open to ${url_string}/${__envConfig.database.name}`);
 });
 
 import account from '../_models/account.model.js';
