37fb8997ecedc05da9ae05105eaf3571d039423c
Jacques Fourie <jacques@fourie.ca>
2024-10-01 11:41:20 -0600

    Added admin task api endpoints

--- jest.config.ts
+++ jest.config.ts
@@ -7,10 +7,14 @@ const config: Config.InitialOptions = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   testMatch: ['**/*.test.ts'],
+  transformIgnorePatterns: [
+    "node_modules/(?!typesit/.*)"
+  ],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
       tsConfig: 'tsconfig.json',
-    }]
+    }],
+    "node_modules/typesit/.+\\.(j|t)sx?$": "ts-jest",
   },
   collectCoverageFrom: [
     '**/*.{ts,tsx}',
--- src/admin/service.ts
+++ src/admin/service.ts
@@ -1,6 +1,7 @@
 import db from '../_helpers/db.js';
 import transaction from '../_helpers/transaction.js';
 import { ITransaction, ITransactionForm } from 'typesit';
+import { tasks } from '../_tasks/task.js';
 
 const Account = db.account
 const Product = db.product
@@ -14,7 +15,67 @@ async function getAllTransactions() {
     return await transaction.getAll();
 }
 
+interface ITask {
+    name: string;
+    stopped: boolean;
+    lastRun: Date | null;
+    nextRun: Date | null;
+}
+
+async function getAllTasks() {
+    // Serialize tasks
+    return tasks.map(task => {
+        return {
+            name: task.name,
+            stopped: task.stopped,
+            lastRun: task.lastRun,
+            nextRun: task.nextRun
+        }
+    });
+}
+
+async function getTask(name: string) {
+    const task = tasks.find(task => task.name === name);
+    if (task === undefined) {
+        throw 'task not found';
+    }
+    return {
+        name: task.name,
+        stopped: task.stopped,
+        lastRun: task.lastRun,
+        nextRun: task.nextRun
+    }
+
+}
+
+async function startTask(name: string) {
+    const task = tasks.find(task => task.name === name);
+    task?.start();
+}
+
+async function stopTask(name: string) {
+    const task = tasks.find(task => task.name === name);
+    task?.stop();
+}
+
+async function forceRunTask(name: string) {
+    const task = tasks.find(task => task.name === name);
+    task?.forceRun();
+}
+
+async function getTaskLogs(name: string) {
+    const task = tasks.find(task => task.name === name);
+    // return task?.logs;
+}
+
+
 export default {
     createTransaction,
-    getAllTransactions
+    getAllTransactions,
+    getAllTasks,
+    getTask,
+    startTask,
+    stopTask,
+    forceRunTask,
+    getTaskLogs
 }
